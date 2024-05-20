27740e7356435a775ede328456a72e8fe66053cf
Linf0rd <linfordsnagger@gmx.com>
2024-05-20 08:44:35 +0200

    "Updated App.jsx to import Home from Home component, not Cart. Also, updated Cart.jsx to import Cart.css instead of Home.css."

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Cart/Cart'
+import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import Order from './pages/Order/Order'
 
