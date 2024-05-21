b8223b51b61ae62658b21c6fe9dcf1b9bd58bd08
Linf0rd <linfordsnagger@gmx.com>
2024-05-21 10:13:42 +0200

    "Updated favicon, navbar logo width, and added animation to index.css; added Header component to Home page"

---
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+      <img src="./header_food.png" alt="Header"/>
+        <h2>Discover your new favorite dish.</h2>
+        <p> Explore our extensive menu, featuring a variety of dishes made with fresh, high-quality ingredients. Our goal is to provide you with an unforgettable dining experience that satisfies your taste buds and leaves you craving more.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
