e0b25ac1df24f1fa47fad6314d7ae5fa5c35f35c
Linf0rd <linfordsnagger@gmx.com>
2024-05-23 18:27:53 +0200

    "Updated Header component CSS and JSX: changed img selector and styles, moved img element outside of header-content div"

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
+      <img src="./header_food.png" alt="Header_food"/>
       <div className="header-content">
-      <img src="./header_food.png" alt="Header"/>
         <h2>Discover your new favourite dish.</h2>
         <p> Explore our extensive menu, featuring a variety of dishes made with fresh, high-quality ingredients. Our goal is to provide you with an unforgettable dining experience that satisfies your taste buds and leaves you craving more.</p>
         <button>View Menu</button>
