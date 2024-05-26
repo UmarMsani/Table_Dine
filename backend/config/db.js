15081b5856cf6d37d830e1d37dccaf62025b8a1d
Linf0rd <linfordsnagger@gmx.com>
2024-05-26 04:06:22 +0200

    "Added database connection and middleware configuration to server.js"

---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+export const connectDB = async() => {
+    await mongoose.connect('mongodb+srv://linford:AdminNimdA@cluster0.kxs5rub.mongodb.net/table-dine').then(()=>console.log("DB Connected"));
+}
